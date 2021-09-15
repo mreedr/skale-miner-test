a4d4ac66ac2566b21df91454a02e1b200713a4e6
mreedr <michael@waly.cash>
2021-09-15 13:21:27 -0400

    update

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ require('dotenv').config()
 let ethers = require('ethers')
 let BN = ethers.BigNumber
 let Web3 = require('web3')
+
 // web3 = new Web3(process.env.INFURA_ENDPOINT);
 web3 = new Web3(process.env.SKALE_ENDPOINT);
 // let ethersProvider = new ethers.providers.JsonRpcProvider(process.env.INFURA_ENDPOINT)
@@ -18,8 +19,9 @@ let payerAbi = JSON.parse('[ { "inputs": [], "stateMutability": "payable", "type
 let payerBytecode = "0x608060405261014c806100136000396000f3fe60806040526004361061002d5760003560e01c80630c11dedd1461003657806312065fe01461007a57610034565b3661003457005b005b6100786004803603602081101561004c57600080fd5b81019080803573ffffffffffffffffffffffffffffffffffffffff1690602001909291905050506100a5565b005b34801561008657600080fd5b5061008f6100f7565b6040518082815260200191505060405180910390f35b8073ffffffffffffffffffffffffffffffffffffffff166108fc670de0b6b3a76400009081150290604051600060405180830381858888f193505050501580156100f3573d6000803e3d6000fd5b5050565b60003073ffffffffffffffffffffffffffffffffffffffff163190509056fea26469706673582212209d96fa3f8ceebc1f6b77e3efcf549b29df42a18bd96fd90b298e5391f504821e64736f6c63430007040033"
 
 async function main() {
-  // let contract = new ethers.Contract('0x3fca0a6295d42f5b87eac3fed4c2364361c46d17', payerAbi, ethersProvider)
-  // console.log('payer contract', (await contract.getBalance()).toString())
+  // let payerContr = new ethers.Contract(process.env.PAYER_ADDR, payerAbi, ethersProvider)
+  // console.log('payer contract', (await payerContr.getBalance()).toString())
+  // await doWeb3Mine(payerContr.address, userPK)
   let contractAddr = await deployPayer(web3, skaleOwnerPK)
   await doWeb3Mine(contractAddr, userPK)
 }
